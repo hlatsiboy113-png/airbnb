8eebf493580edb9a05832b0cffccdd4517d2b1b6
hlatsiboy113-png <hlatsiboy113@gmail.com>
2026-09-01 12:30:34 +0200

    fix: centralize frontend backend URL configuration

--- admin-frontend/src/utils/api.js
+++ admin-frontend/src/utils/api.js
@@ -1,3 +1,7 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api', headers: { 'Content-Type': 'application/json' } });
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+export const BACKEND_URL = API_URL.replace(/\/api\/?$/, '');
+
+const api = axios.create({ baseURL: API_URL, headers: { 'Content-Type': 'application/json' } });
 export default api;
